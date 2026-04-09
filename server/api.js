d64e22bc497bc574c44168487369b6277d9bd84a
PlayboiMagnus <152646957+PlayboiMagnus@users.noreply.github.com>
2026-04-09 13:39:37 +0200

    index v5 (modified api.js for functionality)

--- server/api.js
+++ server/api.js
@@ -9,6 +9,16 @@ const PORT = 3000;
 // Middleware
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+  next();
+});
+
 // Initial mock data
 const initialDeals = [
   {
